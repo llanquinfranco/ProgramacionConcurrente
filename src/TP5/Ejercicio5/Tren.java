8d03213c5bf58ddfbcd600e79e8814755c7d58a5
llanquinfranco <llanquinf@gmail.com>
2023-10-22 17:57:26 -0300

    arrancar trenes

--- src/TP5/Ejercicio5/Tren.java
+++ src/TP5/Ejercicio5/Tren.java
@@ -14,41 +14,5 @@ public class Tren {
     private Semaphore semViaje;
     private Semaphore semSalida;
 
-    public Tren(int espacio) {
-        this.espacio = espacio;
-        this.semTicket = new Semaphore(0);
-        this.semAcceso = new Semaphore(0);
-        this.semViaje = new Semaphore(0);
-        this.semSalida = new Semaphore(0);
-    }
-
-    public void comprarTicket(int numero) {
-        System.out.println("El Cliente " + numero + " quiere comprar un ticket");
-        semTicket.release();
-    }
-
-    public void venderTicket() throws InterruptedException {
-        semTicket.acquire();
-        System.out.println("El Vendedor de Tickets vendio un ticket");
-        semAcceso.release();
-    }
-
-    public void subirAlTren(int numero) throws InterruptedException {
-        semAcceso.acquire();
-        System.out.println("El Cliente " + numero + " ocupo un lugar del tren");
-        semViaje.release();
-    }
     
-    public void iniciarRecorrido() throws InterruptedException {
-        semViaje.acquire(espacio);
-        System.out.println("El Tren Turistico inicio el Recorrido");
-        Thread.sleep(1000);
-        System.out.println("El Tren Turistico finalizo el Recorrido");
-        semSalida.release(espacio);
-    }
-    
-    public void bajarseDelTren(int numero) throws InterruptedException {
-        semSalida.acquire();
-        System.out.println("El Cliente " + numero + " salio del Tren");
-    }
 }
