024d56d4c8dfcfb1a451cd1f3312a0d53f33e565
llanquinfranco <llanquinf@gmail.com>
2023-11-28 12:51:06 -0300

    plantear buffer oscilante con locks

---
+++ src/Parciales/BufferOscilanteLocks/Buffer.java
@@ -0,0 +1,51 @@
+package Parciales.BufferOscilanteLocks;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ *
+ * @author Fran
+ */
+public class Buffer {
+    
+    private int colaExtraer;
+    private Queue<Object> cola1;
+    private Queue<Object> cola2;
+    
+    
+    public Buffer() {
+        this.colaExtraer = 2;
+        this.cola1 = new LinkedList<>();
+        this.cola2 = new LinkedList<>();
+        
+        
+        
+    }
+    
+    
+    
+    public void insertar(Object elem) {
+        
+        
+        
+    }
+    
+    
+    
+    public void extraer() {
+        
+        
+        
+        
+    }
+    
+    
+    
+    private void oscilar() {
+        
+        
+        
+        
+    }
+}
---
+++ src/Parciales/BufferOscilanteLocks/Extractor.java
@@ -0,0 +1,29 @@
+package Parciales.BufferOscilanteLocks;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Fran
+ */
+public class Extractor extends Thread {
+
+    Random random = new Random();
+    private Buffer buffer;
+
+    public Extractor(String nombre, Buffer buffer) {
+        super(nombre);
+        this.buffer = buffer;
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                buffer.extraer();
+                Thread.sleep(random.nextInt(1000, 2000));
+            } catch (Exception ex) {
+
+            }
+        }
+    }
+}
